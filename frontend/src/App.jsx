009c6159b7fe5c5d302000626a5f17992cf340ae
Muhammadyusuf <muhammadyusufabdukarimov313@gmail.com>
2026-01-17 19:07:25 +0500

    edit app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./pages/Header/Header";
 import Footer from "./pages/Footer/Footer";
-import SignIn from "./pages/Auth/SignIn";
+// import SignIn from "./pages/Auth/SignIn";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Footer />
-      <SignIn />
+      {/* <SignIn /> */}
     </div>
   );
 };
