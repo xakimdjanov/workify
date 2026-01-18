1002fc7bd1123c728bdd8dd8f6eaaa3cfa73e449
isomiddinking <isomiddinxakimjanov54@gmail.com>
2026-01-18 19:43:07 +0500

    edit sidebar

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -9,7 +9,7 @@ const MainLayout = () => {
       <Sidebar />
 
       {/* Main content */}
-      <main className="flex-1 p-4 ml-64 md:p-6 talent-sidebar">
+      <main className="flex-1 p-4 ml-[290px] md:p-6 talent-sidebar">
         <Outlet />
       </main>
     </div>
